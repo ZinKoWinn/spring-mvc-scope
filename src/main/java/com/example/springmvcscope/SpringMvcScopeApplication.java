3fa136c861c315aa43740b81e69a6fb8560e7edd
ZinKoWinn <61304140+ZinKoWinn@users.noreply.github.com>
2021-06-04 08:02:24 +0630

    Initial commit

---
+++ src/main/java/com/example/springmvcscope/SpringMvcScopeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcScopeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springmvcscope/beans/RequestScopeBean.java
@@ -0,0 +1,13 @@
+package com.example.springmvcscope.beans;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@RequestScope
+@Component
+@Data
+public class RequestScopeBean {
+    private Integer value;
+
+}
